e2f5ce035d228b3cb822fcb00e8c5a17a4ce4d5c
Yuan Gao <gaoyuankidult@gmail.com>
2017-01-23 09:26:43 +0100

    Changed the default path of static content.

--- motor-blog/motor_blog/application.py
+++ motor-blog/motor_blog/application.py
@@ -28,62 +28,62 @@ def get_application(root_dir, db, option_parser):
         U(r"/api", APIHandler, name='api'),
         U(r"/rsd", RSDHandler, name='rsd'),
 	# Test
-	(r"/static/(.*)", web.StaticFileHandler, {"path": "web-static"}),	
+    (r"/(.*)", web.StaticFileHandler, {"path": "web-static"}),
         # Admin
-        U(r"admin/?", LoginHandler, name='login'),
-        U(r"admin/logout/?", LogoutHandler, name='logout'),
-        U(
-            r"admin/categories/?",
-            CategoriesAdminHandler, name='categories-page'),
-        U(
-            r"admin/categories/delete",
-            DeleteCategoryHandler, name='delete-category'),
+        #U(r"admin/?", LoginHandler, name='login'),
+        #U(r"admin/logout/?", LogoutHandler, name='logout'),
+        #U(
+        #    r"admin/categories/?",
+        #    CategoriesAdminHandler, name='categories-page'),
+        #U(
+        #    r"admin/categories/delete",
+        #    DeleteCategoryHandler, name='delete-category'),
 
-        U(r"admin/drafts/?", DraftsHandler, name='drafts'),
-        U(
-            r"admin/drafts/add-guest-access-token",
-            AddDraftGuestAccessTokenHandler, name='add-guest-access-token'),
+        #U(r"admin/drafts/?", DraftsHandler, name='drafts'),
+        #U(
+        #    r"admin/drafts/add-guest-access-token",
+        #    AddDraftGuestAccessTokenHandler, name='add-guest-access-token'),
 
-        U(
-            r"admin/drafts/delete-guest-access-token",
-            DeleteDraftGuestAccessTokenHandler, name='delete-guest-access-token'),
+        #U(
+        #    r"admin/drafts/delete-guest-access-token",
+        #    DeleteDraftGuestAccessTokenHandler, name='delete-guest-access-token'),
 
-        U(r"admin/draft/(?P<slug>.+)/?", DraftHandler, name='draft'),
+        #U(r"admin/draft/(?P<slug>.+)/?", DraftHandler, name='draft'),
 
-        U(r"admin/media/?", MediaPageHandler, name='media-page'),
-        U(r"admin/media/delete", DeleteMediaHandler, name='delete-media'),
-        U(
-            r"admin/static/(.+)",
-            StaticFileHandler, {"path": admin_static_path}),
+        #U(r"admin/media/?", MediaPageHandler, name='media-page'),
+        #U(r"admin/media/delete", DeleteMediaHandler, name='delete-media'),
+        #U(
+        #    r"admin/static/(.+)",
+        #    StaticFileHandler, {"path": admin_static_path}),
 
         # Atom
-        U(r"feed/?", FeedHandler, name='feed'),
-        U(r"category/(?P<slug>.+)/feed/?", FeedHandler, name='category-feed'),
+        #U(r"feed/?", FeedHandler, name='feed'),
+        #U(r"category/(?P<slug>.+)/feed/?", FeedHandler, name='category-feed'),
 
         # Web
-        U(r"media/(.+)", GridFSHandler, {"database": db}, name='media'),
-        U(
-            r"analytics/(?P<path>.+)/pixel.gif",
-            TrackingPixelHandler, name='tracking-pixel'
-        ),
-        U(
-            r"theme/static/(.+)",
-            StaticFileHandler, {"path": static_path}, name='theme-static'),
-        U(
-            r"category/(?P<slug>.+)/page/(?P<page_num>\d+)/?",
-            CategoryHandler, name='category-page'),
-        U(r"category/(?P<slug>.+)/?", CategoryHandler, name='category'),
-        U(r"page/(?P<page_num>\d+)/?", RecentPostsHandler, name='page'),
-        U(r"all-posts/?", AllPostsHandler, name='all-posts'),
-        U(
-            r"all-posts/page/(?P<page_num>\d+)/?",
-            AllPostsHandler,
-            name='all-posts-page'),
-        U(
-            r"tag/(?P<tag>.+)/page/(?P<page_num>\d+)/?",
-            TagHandler, name='tag-page'),
-        U(r"tag/(?P<tag>.+)/?", TagHandler, name='tag'),
-        U(r"search/", SearchHandler, name='search'),
+        #U(r"media/(.+)", GridFSHandler, {"database": db}, name='media'),
+        #U(
+        #    r"analytics/(?P<path>.+)/pixel.gif",
+        #    TrackingPixelHandler, name='tracking-pixel'
+        #),
+        #U(
+        #    r"theme/static/(.+)",
+        #    StaticFileHandler, {"path": static_path}, name='theme-static'),
+        #U(
+        #    r"category/(?P<slug>.+)/page/(?P<page_num>\d+)/?",
+        #    CategoryHandler, name='category-page'),
+        #U(r"category/(?P<slug>.+)/?", CategoryHandler, name='category'),
+        #U(r"page/(?P<page_num>\d+)/?", RecentPostsHandler, name='page'),
+        #U(r"all-posts/?", AllPostsHandler, name='all-posts'),
+        #U(
+        #    r"all-posts/page/(?P<page_num>\d+)/?",
+        #    AllPostsHandler,
+        #    name='all-posts-page'),
+        #U(
+        #    r"tag/(?P<tag>.+)/page/(?P<page_num>\d+)/?",
+        #    TagHandler, name='tag-page'),
+        #U(r"tag/(?P<tag>.+)/?", TagHandler, name='tag'),
+        #U(r"search/", SearchHandler, name='search'),
     ]
 
     home_slug = option_parser.home_page
